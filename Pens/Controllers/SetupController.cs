a01900a1ca0387733dfcb6f43dfdc7edb5aae3a8
Samuel Ketels <ketels.samuel@gmail.com>
2020-08-05 16:21:34 +0200

    setup pages

---
+++ Pens/Controllers/SetupController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pens.Controllers
+{
+    public class SetupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EditFillSystems()
+        {
+            return View();
+        }
+
+        public IActionResult EditPenMatters()
+        {
+            return View();
+        }
+
+        public IActionResult EditPenNibs()
+        {
+            return View();
+        }
+
+        public IActionResult EditPenPoints()
+        {
+            return View();
+        }
+
+        public IActionResult EditPenStates()
+        {
+            return View();
+        }
+    }
+}
